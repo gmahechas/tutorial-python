954ac340c07797aa66960189c368ed95b7d99523
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-01-20 23:08:03 -0500

    python

--- flask-interactive/flaskr/__init__.py
+++ flask-interactive/flaskr/__init__.py
@@ -18,35 +18,14 @@
 # def hello(name=None):
 #     return render_template('hello.html', name=name)
 
-import os
-
 from flask import Flask
+from .routes import init_routes
 
+def create_app():
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route("/me")
-    def me():
-        return "Hello, World!"
+    app = Flask(__name__)
+    
+    with app.app_context():
+        init_routes()
 
     return app
