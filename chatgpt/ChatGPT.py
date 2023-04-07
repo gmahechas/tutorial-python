5a719d4a78a4b16b451f9a46180fdfac1fb73650
Gustavo Mahecha <gmahechas@ingenieros.com>
2023-04-06 20:58:24 -0500

    trash

--- chatgpt/ChatGPT.py
+++
@@ -1,9 +0,0 @@
-import openai
-class ChatGPT:
-
-    def __init__(self, organization, api_key):
-        openai.organization = organization
-        openai.api_key = api_key
-
-    def get_models(self):
-        return openai.Model.list()
