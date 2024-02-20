aee72f796c4762784b96a5deeba0139ba3ed2f5d
Gustavo Mahecha <gmahechas@outlook.com>
2024-02-19 23:26:25 -0500

    python

---
+++ e_data_types/main.py
@@ -0,0 +1,29 @@
+# integers
+a = 10
+print(a)
+b = -7
+print(b)
+
+# floats
+c: float = 10.5
+print(c)
+d: float = -7.5
+print(d)
+
+# boolean
+e: bool = True
+
+# char
+f: str = "a"
+
+# tuple
+g: tuple = (1, 2, "tavogus", [1, 2, 3], (1, 2)), {"key": "value"}
+print(g)
+
+# array === list
+h: list = [1, 2, "tavogus", [1, 2, 3], (1, 2)]
+print(h)
+
+# dictionary
+i: dict = {"key": "value"}
+print(i)
